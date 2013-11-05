dd07446c84ba581dbf573ddc6db699ed2064b118
atiraj <katiraj@gmail.com>
2013-11-04 20:57:01 -0500

    Added batch method

--- SFRESTConnector/SFRestBulkOperation.cs
+++ SFRESTConnector/SFRestBulkOperation.cs
@@ -27,6 +27,12 @@ namespace SFNetRestLib
             set;
         }
 
+        private static string jobUri
+        {
+            get;
+            set;
+        }
+
         public static void CreateJob(string sfSessionId, string sfOperation, string sfObjectName, ref string jobId)
         {
                 string str = string.Empty;
@@ -38,6 +44,8 @@ namespace SFNetRestLib
                 Stream responseStream = null;
                 try
                 {
+                    CheckConfigurationSetting();
+                    schemaFile = AppDomain.CurrentDomain.BaseDirectory + schemaDir + "\\" + sfObjectName + "_Job.xml";
                     if(schemaDir==string.Empty || schemaDir==null)
                     {
                         schemaDir = ConfigurationManager.AppSettings.Get("schemaDir");
@@ -57,7 +65,7 @@ namespace SFNetRestLib
                     //str = "" + "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">" + "    <operation></operation>" + "    <object></object>" + "    <contentType>CSV</contentType>" + "</jobInfo>";
                     //Eg for XML content type
                     //"    <contentType>XML</contentType>" &
-                    reqURL = "https://na15-api.salesforce.com/services/async/28.0/job";
+                    reqURL = jobUri;
                     reqDoc = new XmlDocument();
                     //reqDoc.LoadXml(str);
                     // added XML modifications
@@ -92,11 +100,74 @@ namespace SFNetRestLib
         /// <param name="sfResultId"></param>
         /// <remarks></remarks>
 
-        public static void AddBatch(string sfSessionId, byte[] fileBytes, string sfJobId, string sfBatchId = null, string sfResultId = null)
+        public static void AddQueryBatch(string sfSessionId, string sfJobId,string sfObjectName, string sfBatchId = null, string sfResultId = null)
         {
-            string requestURI = ("https://login.salesforce.com/services/async/23.0/job/" + (sfJobId + "/batch"));
-            Post(fileBytes, requestURI, sfSessionId, "POST", "text/csv; charset=UTF-8");
+            byte[] fileBytes = null;
+            string str = string.Empty;
+            string reqURL = string.Empty;
+            string schemaFile = string.Empty;
+            XmlDocument reqDoc = null;
+            XmlDocument responseXmlDocument = new XmlDocument();
+            Stream responseStream = null;
 
+            string requestURI = ("https://na15-api.salesforce.com/services/async/28.0/job" + (sfJobId + "/batch"));
+
+            try
+            {
+                    CheckConfigurationSetting();
+                    schemaFile = AppDomain.CurrentDomain.BaseDirectory + schemaDir + "\\" + sfObjectName + "_Batch.xml";
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            try
+            {
+                //str = "" + "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">" + "    <operation></operation>" + "    <object></object>" + "    <contentType>CSV</contentType>" + "</jobInfo>";
+                //Eg for XML content type
+                //"    <contentType>XML</contentType>" &
+                reqURL = jobUri + "/" + sfJobId + "/batch";
+                reqDoc = new XmlDocument();
+                //reqDoc.LoadXml(str);
+                // added XML modifications
+                reqDoc.Load(schemaFile);
+                str=reqDoc.GetElementsByTagName("query")[0].InnerText;
+                fileBytes = System.Text.Encoding.ASCII.GetBytes(str);
+                //bytes = System.Text.Encoding.UTF8.GetBytes(reqDoc.InnerXml)
+
+                using (responseStream = Post(fileBytes, reqURL, sfSessionId, "POST", "application/xml; charset=UTF-8"))
+                {
+                    responseXmlDocument.Load(responseStream);
+                    //Get jobId
+                    sfBatchId = ((((responseXmlDocument) != null)) ? responseXmlDocument.GetElementsByTagName("id").Item(0).InnerText : "");
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string GetBatchResult(string sfSessionId, string sfJobId, string sfObjectName, string sfBatchId, string sfResultId = null)
+        {
+            string reqUrl = string.Empty;
+            string str = string.Empty;
+            Stream responseStream = null;
+            XmlDocument responseXmlDocument = new XmlDocument();
+
+            CheckConfigurationSetting();
+            reqUrl = jobUri + "/" + sfJobId + "/batch/" + sfBatchId + "/result";
+            using (responseStream = Post(null, reqUrl, sfSessionId, "POST", "application/xml; charset=UTF-8"))
+            {
+                responseXmlDocument.Load(responseStream);
+                //Get jobId
+                sfResultId = ((((responseXmlDocument) != null)) ? responseXmlDocument.GetElementsByTagName("id").Item(0).InnerText : "");
+            }
+            return sfResultId;
         }
 
         /// <summary>
@@ -166,6 +237,35 @@ namespace SFNetRestLib
             //Return response Stream
             WebResponse responseHttpRequest = requestHttp.GetResponse();
             return responseHttpRequest.GetResponseStream();
+        }
+
+        static void CheckConfigurationSetting()
+        {
+            try
+            {
+                if (jobUri == string.Empty || jobUri == null)
+                {
+                    jobUri = ConfigurationManager.AppSettings.Get("sfdcJobUri");
+                    if (jobUri == string.Empty || jobUri == null)
+                    {
+                        throw new Exception("Please provide valid value for Job API URI in app settings.");
+                    }
+                }
+
+                if (schemaDir == string.Empty || schemaDir == null)
+                {
+                    schemaDir = ConfigurationManager.AppSettings.Get("schemaDir");
+                    if (schemaDir == string.Empty || schemaDir == null)
+                    {
+                        throw new Exception("Please provide valid value for schemaDir in app settings.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
     }
 }
